b0bcabdf28c9f4113e69c7947cfce81baaa052df
yamahalion <0101k.yamahara@gmail.com>
2020-10-24 19:20:39 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,6 @@ class PostsController < ApplicationController
   end
 
   def checked
-    binding.pry
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
